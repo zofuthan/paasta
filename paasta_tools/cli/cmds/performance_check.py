8a97617f21d0a228f7dae06e90f71c2e27ca453b
Kyle Anderson <kwa@yelp.com>
2016-10-13 14:15:48 -0700

    Added back in a commit option to the performance-check command

--- paasta_tools/cli/cmds/performance_check.py
+++ paasta_tools/cli/cmds/performance_check.py
@@ -32,6 +32,10 @@ def add_subparser(subparsers):
              'Jenkins job name, will be stripped.',
     )
     list_parser.add_argument(
+        '-k', '--commit',
+        help='Git commit sha.',
+    )
+    list_parser.add_argument(
         '-d', '--soa-dir',
         dest='soa_dir',
         metavar='SOA_DIR',
@@ -49,7 +53,7 @@ def load_performance_check_config(service, soa_dir):
     )
 
 
-def submit_performance_check_job(service, soa_dir):
+def submit_performance_check_job(service, commit, soa_dir):
     performance_check_config = load_performance_check_config(service, soa_dir)
 
     if not performance_check_config:
