d87345b51d7a30edfdb1829b64268af139e13a45
Matthew Smith <matts@yelp.com>
2016-11-14 17:22:19 -0800

    rebased and fixed tests

--- paasta_tools/deployment_utils.py
+++ paasta_tools/deployment_utils.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from paasta_tools.utils import DEFAULT_SOA_DIR
-from paasta_tools.utils import load_v2_deployments_json
+from paasta_tools.utils import load_deployments_json
 from paasta_tools.utils import NoDeploymentsAvailable
 
 
@@ -21,7 +21,7 @@ def get_currently_deployed_sha(service, deploy_group, soa_dir=DEFAULT_SOA_DIR):
     """Tries to determine the currently deployed sha for a service and deploy_group,
     returns None if there isn't one ready yet"""
     try:
-        deployments = load_v2_deployments_json(service=service, soa_dir=soa_dir)
+        deployments = load_deployments_json(service=service, soa_dir=soa_dir)
         return deployments.get_git_sha_for_deploy_group(deploy_group=deploy_group)
     except NoDeploymentsAvailable:
         return None
